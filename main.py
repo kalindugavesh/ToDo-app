b7669dcfccfa24617faf117564c853ba70020f5a
Kalindu Gavesh <kalindugaveshmandara@gmail.com>
2025-12-26 19:34:57 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.geometry("400x500")
+root.title("ToDo")
+root.iconbitmap("icon.ico")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#-----------------------main_loop----------------------------#
+root.mainloop()
